1de7d398f26ce49886b23e36a0b6c753d9693c80
AlejoGDOfficial <alejogdoff@gmail.com>
2025-09-20 14:45:34 -0500

    .25 cherea

--- states/TestState.lua
+++ states/TestState.lua
@@ -1,21 +1,13 @@
 local TestState = ALEState:extend()
 
 local spr = ALESprite:new(0, 0, 'dni')
-spr.scale.x, spr.scale.y = 0.5, 0.5
+spr.scale.x, spr.scale.y = 0.25, 0.25
 spr:screenCenter()
 
 function TestState:load()
     self.super:load()
 
     self:add(spr)
-end
-
-local curTime = 0
-
-function TestState:update(elapsed)
-    curTime = curTime + elapsed
-
-    self.super:update(elapsed)
 end
 
 return TestState
