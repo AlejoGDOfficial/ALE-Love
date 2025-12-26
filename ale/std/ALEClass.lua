63c0faa4f382844ff00abec0d631a0c29efbbef8
AlejoGDOfficial <alejogdoff@gmail.com>
2025-12-26 12:46:30 -0500

    hmm

--- ale/std/ALEClass.lua
+++ ale/std/ALEClass.lua
@@ -4,19 +4,15 @@ function ALEClass:new()
     local obj = {
         __getters = {},
         __setters = {},
-        __class   = self
+        __class = self
     }
 
     setmetatable(obj, {
         __index = function(self, key)
             local getter = self.__getters[key]
+
             if getter then
                 return getter(self)
-            end
-
-            local value = rawget(self, key)
-            if value ~= nil then
-                return value
             end
 
             return self.__class[key]
@@ -24,6 +20,7 @@ function ALEClass:new()
 
         __newindex = function(self, key, value)
             local setter = self.__setters[key]
+
             if setter then
                 return setter(self, value)
             end
@@ -32,7 +29,7 @@ function ALEClass:new()
         end,
 
         __tostring = function(self)
-            return self.__class.__id or "Object"
+            return self.__class.__id
         end
     })
 
@@ -43,16 +40,14 @@ function ALEClass.extend(super)
     super = super or ALEClass
 
     local child = {}
-    child.__super = super
+
+    child.super = super
 
     setmetatable(child, { __index = super })
 
     local info = debug.getinfo(2, 'S')
-    child.__id =
-        (info and info.source or '')
-        :gsub('^@', '')
-        :match('([^/\\]+)%.lua$')
-        or 'Object'
+
+    child.__id = (info and info.source or ''):gsub('^@', ''):match('([^/\\]+)%.lua$') or 'Object'
 
     return child
 end
